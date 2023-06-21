bfbed153f0c3caf2d2166c85540d89cca575d139
Tevin Musira <tevin.musira@gmail.com>
2023-06-21 05:16:01 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point of fn that prints using puts
+ *
+ * Return: 0 (shows success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

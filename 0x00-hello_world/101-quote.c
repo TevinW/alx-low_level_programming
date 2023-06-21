de07a0a67f825959f0a47a6653a2ec733f3957ec
Tevin Musira <tevin.musira@gmail.com>
2023-06-21 06:49:10 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
- * main - entry point for function that prints to stderr
+ * main - entry point for function that
+ * prints to stderr
  *
  * Return: 1 (shows success)
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar
+, 2015-10-19\n");
 	return (1);
 }
 

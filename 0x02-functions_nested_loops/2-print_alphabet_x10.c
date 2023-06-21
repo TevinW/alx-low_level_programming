eaca61d3a7ca15a8803c5bce992865a0b77de868
Tevin Musira <tevin.musira@gmail.com>
2023-06-21 14:45:02 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ * in lowercase followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 0; i++)
+	{
+		char ch;
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		putchar('\n');
+	}
+}
+
+

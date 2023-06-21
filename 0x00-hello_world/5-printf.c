85a7276fa8ade1581a25116cd78a41bb691f9b2f
Tevin Musira <tevin.musira@gmail.com>
2023-06-21 05:26:02 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point for function that prints using printf
+ *
+ * Return: 0 (shows success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+

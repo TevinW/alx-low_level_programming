23695b5e533733a60b1d0f20d94c26818b78801e
Tevin Musira <tevin.musira@gmail.com>
2023-06-21 14:30:12 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,12 @@
 /**
  * main - function that prints the alphabet in
  * lowercase followed by a new line
- *
- * Return: 0 (shows success)
  */
-int main(void)
+void print_alphabet(void)
 {
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
-	return (0);
+	_putchar('\n');
 }

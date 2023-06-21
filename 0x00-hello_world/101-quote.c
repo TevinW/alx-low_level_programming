5f200903ba7b7abbd84c0cc9da349967eb80c2a0
Tevin Musira <tevin.musira@gmail.com>
2023-06-21 06:38:28 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point for function that prints to stderr
+ *
+ * Return: 1 (shows success)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
+

d33538a53f591957d1247ef2d642bc4013b375e0
Tevin Musira <tevin.musira@gmail.com>
2023-06-21 19:55:59 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints sum of all multiples
+ * of 3 and 5 up to 1024
+ * 
+ * Return: sum
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			j += i;
+		}
+	}
+	printf("%d\n", j);
+	return (0);
+}
+

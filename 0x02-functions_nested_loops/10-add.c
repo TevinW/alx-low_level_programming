ec24b477778ff1d1147f0d3450ffc40739b4e4c3
Tevin Musira <tevin.musira@gmail.com>
2023-06-21 18:33:27 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - function that adds two integers
+ * and returns the result
+ * @a: first int
+ * @b: second int
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}

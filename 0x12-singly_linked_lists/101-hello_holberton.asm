23809d8a640708ffe278dc08d946faa220778cdb
Tevin Musira <tevin.musira@gmail.com>
2023-07-27 05:43:02 -0700

    end

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -8,14 +8,15 @@ section .text
 global _start
 
 _start:
-
+    ; Set up the stack frame for the printf call
     push rbp
     mov rdi, format
     mov rsi, hello
-    xor rax, rax
+    xor rax, rax ; Clear rax register to indicate no vector registers used
     call printf
 
+    ; Clean up and exit
     pop rbp
-    mov rax, 60
-    xor edi, edi
+    mov rax, 60         ; syscall: sys_exit
+    xor edi, edi        ; status: 0
     syscall

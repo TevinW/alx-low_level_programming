cdba955d2aea94070af86da94180cd29444cfb42
Tevin Musira <tevin.musira@gmail.com>
2023-07-27 06:02:08 -0700

    edit asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,18 @@
+extern printf
+
 section .data
     message: db 'Hello, Holberton',0
     format: db '%s', 0
 
 section .text
     global main
-    extern printf
 
 main:
 
     push rbp
     mov rdi, format
     mov rsi, message
-    xor rax, rax
     call printf
 
-    pop rbp
-    mov rax, 0
-    xor edi, edi
+    mov rax,0
     ret
-    syscall

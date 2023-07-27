f599479712c5d1eecb5ad4b86fbdf5c08e076b72
Tevin Musira <tevin.musira@gmail.com>
2023-07-27 05:57:01 -0700

    edit asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,20 @@
 section .data
-    hello db 'Hello, Holberton',0
-    format db '%s', 0
+    message: db 'Hello, Holberton',0
+    format: db '%s', 0
 
 section .text
     global main
     extern printf
 
 main:
-    ; Set up the stack frame for the printf call
+
     push rbp
     mov rdi, format
-    mov rsi, hello
-    xor rax, rax ; Clear rax register to indicate no vector registers used
+    mov rsi, message
+    xor rax, rax
     call printf
 
-    ; Clean up and exit
     pop rbp
-    mov rax, 60         ; syscall: sys_exit
-    xor edi, edi        ; status: 0
+    mov rax, 0
+    xor edi, edi
     syscall

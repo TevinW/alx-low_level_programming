aeee87a964db625943acfa6fc8e0c3f4c511456f
TevinW <56083826+TevinW@users.noreply.github.com>
2023-07-31 22:46:29 +0300

    9_edit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new_node->next = *head;
+		new_node->next = *temp;
 		*head = new_node;
 		return (new_node);
 	}

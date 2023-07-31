e4b31a966f88ab26e6b6c35ca591c49178ab0dd4
TevinW <56083826+TevinW@users.noreply.github.com>
2023-07-31 22:42:20 +0300

    10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,32 +8,32 @@
  *
  * Return: 1 (Success), -1 (Failure)
  */
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp = *head;
-	listint_t *current = NULL;
+	listint_t *temp, *copy = *head;
 	unsigned int i;
 
 	if (*head == NULL)
 		return (-1);
 
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(copy);
+		return (1);
+	}
+
 	for (i = 0; i < (index - 1); i++)
-
-		if (index == 0)
-		{
-			*head = (*head)->next;
-			free(temp);
-			return (1);
-		}
-
-		if (!temp || !(temp->next))
+	{
+		if (copy->next == NULL)
 			return (-1);
-		temp = temp->next;
 
+		copy = copy->next;
+	}
 
-	current = temp->next;
-	temp->next = current->next;
-	free(current);
-
+	temp = copy->next;
+	copy->next = temp->next;
+	free(temp);
 	return (1);
 }

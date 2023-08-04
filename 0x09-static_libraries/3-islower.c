34440adc4e7714347e32bfdce0d754b1cc06e7c3
TevinW <56083826+TevinW@users.noreply.github.com>
2023-08-04 16:30:55 +0300

    libmy.a

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - function that checks for lowercase character
+ * @c: parameter to be checked
+ *
+ * Return: 1 (lowercase), 0 (otherwise)
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}

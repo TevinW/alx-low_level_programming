a238375b66d35428177f1bfc0eff5854d6863cbe
TevinW <56083826+TevinW@users.noreply.github.com>
2023-08-04 21:05:56 +0300

    01

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*main - prints n.o of arguments passed to it
+ * 
+ * @argc: n.o of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", (argc - 1));
+
+	return (0);
+}
